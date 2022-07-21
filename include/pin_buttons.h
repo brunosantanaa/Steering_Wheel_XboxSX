a40a57f23824826ccdbc47e06d0a860b054e39ee
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-07-20 23:30:39 -0400

    first

---
+++ include/pin_buttons.h
@@ -0,0 +1,31 @@
+#ifndef PIN_BUTTONS_H
+#define PIN_BUTTONS_H
+
+#define bt_RT    25
+#define bt_RX    17
+#define bt_RY    16
+#define bt_RB    12
+#define bt_R3    39
+
+#define bt_LT    26
+/* bt_LX -> Analog Output HMCP */
+#define bt_LY    19
+#define bt_LB    7
+#define bt_L3    18
+
+#define bt_B     15
+#define bt_Y     2
+#define bt_A     13
+#define bt_X     14
+
+#define bt_R     8
+#define bt_D     4
+#define bt_U     5
+#define bt_L     23
+
+#define bt_XBOX  9
+#define bt_VIEW  10
+#define bt_MENU  11
+#define bt_SHARE 6
+
+#endif //PIN_BUTTONS_H
